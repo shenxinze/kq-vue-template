2c919a6bc7876bfb0c402f0b40f52e34dd23481f
shenxinze <uidshen@163.com>
2024-04-12 16:29:59 +0800

    配置修改

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import locale from 'element-plus/es/locale/lang/zh-cn'
 import '@/assets/css/style.scss'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { createPinia } from 'pinia'
@@ -14,7 +15,7 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-app.use(ElementPlus)
+app.use(ElementPlus, { locale })
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			'@': '/src'
-		}
-	}
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    host: true,
+    proxy: {
+      // '/api': {
+      //   target: 'http://192.168.1.212:8081',
+      //   changeOrigin: true,
+      //   rewrite: (pathStr) => pathStr.replace('/api', '')
+      // }
+    }
+  }
 })
